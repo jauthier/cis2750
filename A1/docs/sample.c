3f40cf7bcab095fd8ca47c1930d068685fd2bf96
jauthier <jauthier@mail.uoguelph.ca>
2017-01-25 16:38:37 -0500

    go

--- A1/docs/sample.c
+++ A1/docs/sample.c
@@ -1,7 +1,19 @@
 #include <stdio.>
 
+class B {
+    int b;
+    
+    int add(int c){
+        int d;
+        printf("%d\n",b+c+d)
+    }
+}
+
 int main(){
     int num = 1;
+    class B myB;
+    
+    myB.add(num);
 
     if (num == 2)
         printf("The number is 2");
