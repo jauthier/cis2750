3d104f7759ecf8c48259b3750e2bee7d1a98d065
jauthier <jauthier@mail.uoguelph.ca>
2017-02-13 16:47:42 -0500

    a2

--- 3490A2/2_3.c
+++ 3490A2/2_3.c
@@ -8,7 +8,7 @@
 #include "2_3.h"
 
 int orientation (Point *p, Point *q, Point *r){
-    int val = (q.y - p.y)*(r.x - q.x)-(q.x - p.x)*(r.y - q.y);
+    int val = (q->y - p->y)*(r->x - q->x)-(q->x - p->x)*(r->y - q->y);
     if (val == 0)
         return 0;
     if (val < 0)
