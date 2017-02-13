5f166268ad0fadad20ea98ec9c58e7f9300da961
jauthier <jauthier@mail.uoguelph.ca>
2017-02-13 15:54:45 -0500

    a2

--- 3490A2/1_3.c
+++ 3490A2/1_3.c
@@ -8,13 +8,14 @@
 
 int countInversions (int * A, int n){
     int count = 0;
-    int i =0;
+    int i = 0;
     for (i=0;i<n;i++){
         int j = 0;
-        for (j=0;j<n;j++){
-            if (A[i]<A[j])
-                count ++;
-        }
+        // for (j=0;j<n;j++){
+        //     if (A[i]<A[j])
+        //         count ++;
+        // }
+        count ++;
     }
     return count;
 }
