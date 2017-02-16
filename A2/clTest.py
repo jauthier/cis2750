3fccb0f1d82066209dc71379b25546517899f205
jauthier <jauthier@mail.uoguelph.ca>
2017-02-16 15:56:19 -0500

    a2

--- A2/clTest.py
+++ A2/clTest.py
@@ -4,7 +4,7 @@ import sys
 
 #get command line agruments
 numArg = int(len(sys.argv))
-for i in range (1,numArg)
+for i in range (1, numArg)
     userName = "%s %s"% (userName, sys.argv[i])
 
 print userName
