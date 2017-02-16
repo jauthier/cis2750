4c3107ae67b0e4d6c44becabd7a68be0bc3293e7
jauthier <jauthier@mail.uoguelph.ca>
2017-02-16 16:33:36 -0500

    a2

--- A2/clTest.py
+++ A2/clTest.py
@@ -11,15 +11,22 @@ if numArg >= 2:
 for i in range (2, numArg):
 	userName = "%s %s" % (userName, sys.argv[i])
 
-print (userName)
-
+#puts all the streams into a list
 allSFile = "messages/allStreams.txt"
 file_1 = open(allSFile,"r")
 allSList = []
 for line in file_1:
 	line = line.rstrip('\n')
 	allSList.append("%s"%line)
-print(allSList)
+
+#check each streamUser file
+userStreams = []
+for stream in allSList:
+	fileName = "messages/%sStreamUsers.txt"%stream
+	print(fileName)
+
+
+
 
 #open a file
 
