80a47a813f7b07490c6019822e5e3cde5fea85c3
jauthier <jauthier@mail.uoguelph.ca>
2017-03-01 19:24:28 -0500

    testpic

---
+++ A3/pictureTest.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*pictures*/
+
+void pictures(char * line){
+    char * src;
+    char * height;
+    char * width;
+
+    char * token = strtok(line,"()");
+    token = strtok(NULL,"()");
+
+    /*token holds the inside of the ()*/
+    char * token2 = strtok(token,",");
+    token = strtok(NULL,",");
+    if (token == NULL){ /*there is no size indicated*/
+        src = strtok(token2,"\"");
+        height = "100";
+        width = "100";
+    } else {
+        /*check which one is image and which one is size*/
+        if (strstr(token,"image")){
+            /*get the stuff between the quotes*/
+            src = strtok(token,"\"");
+            token = strtok(token2,"<>");
+            height = strtok(NULL,"<>");
+            width = strtok(NULL,"<>");
+        } else {
+            src = strtok(token2,"\"");
+            token = strtok(token,"<>");
+            height = strtok(NULL,"<>");
+            width = strtok(NULL,"<>");
+        }
+    }
+
+    char * newLine = malloc(sizeof(char)*(strlen("    <img src=\"")
+        + strlen(src) + strlen("\" height=\"") + strlen(height)
+        + strlen("\" width=\"") + strlen(width) + strlen("\"/>\n")));
+    printf("%s\n", newLine);
+}
+
+int main (int argc, char *argv[]){
+    char hold[] = ".p(src=\"this_pic\", size=<20><30>)";
+    pictures(hold);
+    return 0;
+}
+
