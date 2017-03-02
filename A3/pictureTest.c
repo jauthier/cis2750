7482443fd98e355985a3ea6c6533e7eba395683d
jauthier <jauthier@mail.uoguelph.ca>
2017-03-01 19:38:20 -0500

    testpic

--- A3/pictureTest.c
+++ A3/pictureTest.c
@@ -49,12 +49,16 @@ void pictures(char * line){
     strcat(newLine,"\" width=\"");
     strcat(newLine,width);
     strcat(newLine,"\"/>\n");
-    printf("%s\n", newLine);
+    printf("%s", newLine);
 }
 
 int main (int argc, char *argv[]){
     char hold[] = ".p(image=\"this_pic\", size=<20><30>)";
     pictures(hold);
+    char hold1[] = ".p(image=\"this_pic\")";
+    pictures(hold1);
+    char hold2[] = ".p(size=<20><30>, image=\"this_pic\")";
+    pictures(hold2);
     return 0;
 }
 
