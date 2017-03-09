d246d8defd9e7e38b76b48b4007743c0e73ada6c
jauthier <jauthier@mail.uoguelph.ca>
2017-03-09 18:26:42 -0500

    try again

--- A3/login.php
+++ A3/login.php
@@ -6,27 +6,23 @@
 </head>
 
 <body>
+<h3>Login</h3>
 <?php
+    $username = $Stream = "";
     if ($_SERVER["REQUEST_METHOD"] == "POST"){
-        echo "in if";
-        $username = $_POST["username"];
-        $stream = $_POST["stream"];
-        echo "$username";
-        $command = "./runCSU" . $username . $Stream;
-        exec($command,$output,$return);
-        echo $output[0];
+        if (empty($_POST["username"]) or empty($_POST["stream"])){
+            echo "empty";
+        } else {
+            $username = $_POST["username"];
+            $stream = $_POST["stream"];
+            $command = "./runCSU" . $username . $Stream;
+            exec($command,$output,$return);
+            echo $output[0];
+        }
+        
+        
     }
 ?>
-
-<form action="login.php" method="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    Username: <input type="text" name="username"><br><br>
-    Stream: <input type="text" name="stream"><br><br>
-    <input type="submit" value="Login"><br>
-</form>
-
-
-
-
 <?php if ($output == 1): ?>
 
 <form method="post", action="test.php">
@@ -35,6 +31,12 @@
     <input type="submit">
 </form>
 <?php endif; ?>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    Username: <input type="text" name="username"><br><br>
+    Stream: <input type="text" name="stream"><br><br>
+    <input type="submit" value="Login"><br>
+</form>
 
 </body>
 </html>
