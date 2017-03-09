7c80b49129ef72bf1fce1bd694d6ab44c7fb499a
jauthier <jauthier@mail.uoguelph.ca>
2017-03-09 17:54:04 -0500

    try again

--- A3/login.php
+++ A3/login.php
@@ -3,22 +3,27 @@
 <html>
 <head>
 <title>Login</title>
-<?php
-    $username = $_POST["username"];
-    $stream = $_POST["stream"];
-    $command = "./runCSU" . $username . $Stream;
-    exec($command,$output,$return);
-    echo $output[0];
-?>
-
 </head>
 
 <body>
-<form action="login.php" method="post">
+<form action="login.php" method="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
     Username: <input type="text" name="username"><br><br>
     Stream: <input type="text" name="stream"><br><br>
     <input type="submit" name="login" value="Login"><br>
 </form>
+
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $username = $_POST["username"];
+        $stream = $_POST["stream"];
+        echo "$username";
+        $command = "./runCSU" . $username . $Stream;
+        exec($command,$output,$return);
+        echo $output[0];
+    }
+?>
+
+
 <?php if ($output == 1): ?>
 
 <form method="post", action="test.php">
