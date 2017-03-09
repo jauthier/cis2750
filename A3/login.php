f9fab33df77c673661034d471ddc437c59341d2a
jauthier <jauthier@mail.uoguelph.ca>
2017-03-09 17:24:22 -0500

    try again

--- A3/login.php
+++ A3/login.php
@@ -8,21 +8,19 @@
     $stream = $_POST["stream"];
     $command = "./runCUS" . $username . $Stream;
     exec($command,$output,$return);
-    if (!$return)
-        header("Location:./test.html")
-    else
-        echo "not successful";
-
 ?>
 
 </head>
 
 <body>
-<h3>Login</h3>
-<form action="login.php" method="post">
-    Username: <input type="text" name="username"><br><br>
-    Stream: <input type="text" name="stream"><br><br>
-    <input type="submit" name="login" value="Login"><br>
+<?php if (!$return): ?>
+
+<form method="post", action="test.html">
+    <input type="hidden" name="username" value="$username">
+    <input type="hidden" name="stream" value="$stream">
+    <input type="submit">
 </form>
+<?php endif; ?>
+
 </body>
 </html>
