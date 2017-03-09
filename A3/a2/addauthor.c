eba547efc3ee1662db2e221f2c434a5d17330707
jauthier <jauthier@mail.uoguelph.ca>
2017-03-09 16:11:19 -0500

    try again

--- A3/a2/addauthor.c
+++ A3/a2/addauthor.c
@@ -11,38 +11,22 @@
 
 int main (int argc, char * argv[]){
 
-    if (argc == 1){
-        printf("You must enter a username!\n");
-        exit(0);
-    }
-
     int i = 1;
     int check = 0;
-    char buffer[200];
+    char *buffer;
     /*the user wants to remove a username*/
-    printf("%s\n", argv[1]);
     if (strcmp(argv[1],"-r")==0){
         check = 1;
         i++;
     }
     strcpy(buffer,argv[i]);
     i++;
-    while (i < argc){
-        /*put all strings into one*/
-        strcat(buffer, " ");
-        strcat(buffer, argv[i]);
-        i++;
-    }
     char * username = malloc(sizeof(char)*strlen(buffer));
     strcpy(username,buffer);
 
-    /*get list of streams*/
-    printf("Enter the list of streams you want to add/remove,\nplease separate them by commas:\n");
-    char streamHold[200];
-    fgets(streamHold,200,stdin);
-
-    char * streams = malloc(sizeof(char)*strlen(streamHold));
-    strcpy(streams,streamHold);
+    strcpy(buffer,argv[i]);
+    char * streams = malloc(sizeof(char)*strlen(buffer));
+    strcpy(streams,buffer);
 
     if (check == 1)
         removeUser(username,streams);
