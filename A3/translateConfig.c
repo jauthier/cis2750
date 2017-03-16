b56412d8e23bad46cb68a686a0eb6d37a58a2e01
jauthier <jauthier@mail.uoguelph.ca>
2017-03-16 15:38:29 -0400

    testing config

--- A3/translateConfig.c
+++ A3/translateConfig.c
@@ -122,7 +122,7 @@ void input (char * line, FILE * fp){
     int noValue = 0;
 
     char * token;
-    while (token = strtok(line,","); token != NULL;token = strtok(token+strlen(token)+1,",")){
+    for (token = strtok(line,","); token != NULL; token = strtok(token+strlen(token)+1,",")){
         if (strstr(token,"action") != NULL){
             action = strtok(token,"\"");
             action = strtok(NULL,"\"");
