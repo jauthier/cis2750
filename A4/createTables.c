e75c2f811bd90927bc2d1577d0d1b1bb03722e4f
jauthier <jauthier@mail.uoguelph.ca>
2017-03-31 19:12:16 -0400

    go

--- A4/createTables.c
+++ A4/createTables.c
@@ -141,11 +141,9 @@ int main(int argc, char *argv[]){
         getUsers(mysql);
     } else if (strcmp(action,"-posts")==0){
         printf("in post\n");
-        if (mysql_query(&mysql,"CREATE TABLE cats (stream CHAR(30),user CHAR(30),date CHAR(30),text TEXT(1000) )"))
-            printf("didnt work\n%s\n",mysql_error(&mysql));
-        mysql_query(&mysql,"CREATE TABLE dogs (stream CHAR(30),user CHAR(30),date CHAR(30),text CHAR(1000))");
-        mysql_query(&mysql,"CREATE TABLE cars (stream CHAR(30), user CHAR(30), date CHAR(30), text CHAR(1000))");
-        mysql_query(&mysql, "INSERT INTO cats VALUES ('cats','jess','Mar 31, 2013 6:41:57','Hello\nI am a cat!\n')");
+        mysql_query(&mysql,"CREATE TABLE dogs (stream CHAR(30),user CHAR(30),date CHAR(30),text TEXT(1000))");
+        mysql_query(&mysql,"CREATE TABLE cars (stream CHAR(30), user CHAR(30), date CHAR(30), text TEXT(1000))");
+        mysql_query(&mysql, "INSERT INTO cats VALUES ('cats','jess','Mar 31, 2013 7:11:53','Hello\nWelcome To All Cat Lovers\n')");
         mysql_query(&mysql, "INSERT INTO dogs VALUES ('dogs','luke','Mar 31, 2013 6:43:57','Hello\nI am a Dog!\n')");
         mysql_query(&mysql, "INSERT INTO cars VALUES ('cars','josh','Mar 31, 2013 6:45:57','Hello\nI am a car!\n')");
         getPosts (mysql);
